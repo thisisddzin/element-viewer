f64099a62ba953d007aa908033337448bb48df56
Adenilson Santos <adenilson.santos@gocase.com.br>
2023-03-24 15:06:45 -0300

    first commit

---
+++ element-viewer.js
@@ -0,0 +1,25 @@
+function elementViewer(options = { 
+  elementSelector: 'body', 
+  visualizationTimer: 3000 
+}, callback = function () { 
+  console.log('Element viewed by 3 seconds (default).') 
+}) {
+  var { elementSelector, visualizationTimer } = options
+  var timeout
+  var element = document.querySelector(elementSelector)
+  
+  window.addEventListener('scroll', function () {
+    clearInterval(timeout)
+    timeout = setInterval(function () {
+      var rect = element.getBoundingClientRect()
+      var isVisible = rect.top <= 50 && rect.bottom >= element.clientHeight / 2;
+      console.log('interval closed', isVisible);
+
+      if (isVisible) {
+        callback();
+      } else {
+        clearTimeout(timeout);
+      }
+    }, visualizationTimer);
+  })
+}
