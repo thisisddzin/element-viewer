fd3579ea959ff4d79f609dccc0e7030259ecfd87
Adenilson Santos <adenilson.santos@gocase.com.br>
2023-03-28 09:12:41 -0300

    fix infinity listening

--- src/element-viewer.js
+++ src/element-viewer.js
@@ -4,21 +4,28 @@ function elementViewer(options = {
 }, callback = () => { 
   console.log('Element viewed by 3 seconds (default).') 
 }) {
-  let timeout
+  let interval
+  let keepListening = true
   const { elementSelector, visualizationTimer } = options
   const element = document.querySelector(elementSelector)
   
-  window.addEventListener('scroll', () => {
-    clearInterval(timeout)
-    timeout = setInterval(() => {
+  const listenScrolling = () => {
+    if(!keepListening) return
+    
+    clearInterval(interval)
+    interval = setInterval(() => {
       const rect = element.getBoundingClientRect()
-      const isVisible = rect.top <= 50 && rect.bottom >= element.clientHeight / 2;
+      const isVisible = rect.top <= 50 && rect.bottom >= element.clientHeight / 2
 
       if (isVisible) {
-        callback();
-      } else {
-        clearTimeout(timeout);
+        callback()
+        keepListening = false
+        window.removeEventListener('scroll', this)
       }
-    }, visualizationTimer);
-  })
+
+      clearInterval(interval)
+    }, visualizationTimer)
+  }
+
+  window.addEventListener('scroll', listenScrolling)
 }
